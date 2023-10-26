bff2c3e34fd42508e79f139a64dab46d76f8729e
Tiago Alves <tiagoh_alves@hotmail.com>
2023-10-26 01:54:38 -0300

    Adicionado nomenclatura ct

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,17 +1,16 @@
 /// <reference types="Cypress" />
 
-const { values } = require("cypress/types/lodash");
-
 describe("Central de Atendimento ao Cliente TAT", function () {
   beforeEach(function() {
     cy.visit("./cypress-basico-v2/src/index.html");
+    
   });
 
-  it("verifica o título da aplicação", function () {  
+  it("CT001 verifica o título da aplicação", function () {  
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
   });
 
-  it("preenche os campos obrigatotios e envia o formulario", function () {
+  it("CT002 preenche os campos obrigatotios e envia o formulario", function () {
     const longText = "Gostaria de expressar minha mais profunda gratidão por ter tido a oportunidade de participar do curso. Foi uma jornada incrível, repleta de aprendizado, desafios e crescimento pessoal. "
     cy.get("#firstName").should('be.visible').type("Tiago");
     cy.get("#lastName").should('be.visible').type("Alves");
@@ -22,7 +21,7 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.get('.success > strong').should("be.visible");
   });
 
-  it("exibe mensagem de erro ao submeter o formulário com um email com formatação inválida",function(){
+  it("CT003 exibe mensagem de erro ao submeter o formulário com um email com formatação inválida",function(){
     cy.get("#firstName").should('be.visible').type("Tiago");
     cy.get("#lastName").should('be.visible').type("Alves");
     cy.get("#phone").should('be.visible').type("11980734020");
@@ -31,11 +30,11 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.get('.error > strong').should('be.visible');
   });
 
-  it("verificar que ao enviar letras o campo telefone continua vazio", function(){
+  it("CT004 verificar que ao enviar letras o campo telefone continua vazio", function(){
     cy.get("#firstName").should('be.visible').type("Tiago");
     cy.get("#lastName").should('be.visible').type("Alves");
     cy.get("#email").should('be.visible').type("tiagoh_alves@hotmail.com",);
     cy.get("#phone").should('be.visible').type("Tiago");
-    cy.get('#phone').should('have.value',null);
+    cy.get('#phone').should('have.value','');
   });
 });
