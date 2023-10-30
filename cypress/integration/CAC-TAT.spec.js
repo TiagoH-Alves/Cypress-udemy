1eadee0232a02be63ba9426cbbb6a54bd66ec5a2
Tiago Alves <tiagoh_alves@hotmail.com>
2023-10-30 20:14:34 -0300

    Adicionando novos cenarios

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -66,13 +66,58 @@ describe("Central de Atendimento ao Cliente TAT", function () {
 
   it("CT008 Envia o formulario com comando customizado ",function(){
     cy.fillMandatoryFieldsAndSubmit()
-  })
+  });
 
   it("CT009 clicar no botão enivar utilizando o Contains", function(){
     cy.contains('button','Enviar').click();
     cy.get('.error > strong').should("be.visible");
-  })
+  });
 
+  it("CT010 Enviar o formulario utilizando uma selecao suspença", function(){
+    cy.get("#firstName").should('be.visible').type("Tiago");
+    cy.get("#lastName").should('be.visible').type("Alves");
+    cy.get("#email").should('be.visible').type("tiagoh_alves@hotmail.com",);
+    cy.get("#phone").should('be.visible').type("11980734020");
+    cy.get('#product').select('Cursos').should('have.value','cursos')
+    cy.get("#open-text-area").should('be.visible').type( "muito obrigado pelo curso" , {delay: 0});
+    cy.get('button[type="submit"]').click();
+    cy.get('.success > strong').should("be.visible");
+  });
+
+  it("CT011 Enviar o formulario utilizando uma selecao por valor", function(){
+    cy.get("#firstName").should('be.visible').type("Tiago");
+    cy.get("#lastName").should('be.visible').type("Alves");
+    cy.get("#email").should('be.visible').type("tiagoh_alves@hotmail.com",);
+    cy.get("#phone").should('be.visible').type("11980734020");
+    cy.get('#product').select('mentoria').should('have.value','mentoria')
+    cy.get("#open-text-area").should('be.visible').type( "muito obrigado pelo curso" , {delay: 0});
+    cy.get('button[type="submit"]').click();
+    cy.get('.success > strong').should("be.visible");
+  });
+
+  it("CT012 Enviar o formulario utilizando uma selecao por indice", function(){
+    cy.get("#firstName").should('be.visible').type("Tiago");
+    cy.get("#lastName").should('be.visible').type("Alves");
+    cy.get("#email").should('be.visible').type("tiagoh_alves@hotmail.com",);
+    cy.get("#phone").should('be.visible').type("11980734020");
+    cy.get('#product').select(4).should('have.value','youtube')
+    cy.get("#open-text-area").should('be.visible').type( "muito obrigado pelo curso" , {delay: 0});
+    cy.get('button[type="submit"]').click();
+    cy.get('.success > strong').should("be.visible");
+  });
+
+  it("CT010 Enviar o formulario utilizando uma selecao suspença", function(){
+    cy.get("#firstName").should('be.visible').type("Tiago");
+    cy.get("#lastName").should('be.visible').type("Alves");
+    cy.get("#email").should('be.visible').type("tiagoh_alves@hotmail.com",);
+    cy.get("#phone").should('be.visible').type("11980734020");
+    cy.get('#product').select('Cursos').should('have.value','cursos')
+    cy.get('input[type="radio"][value="feedback"]').check().should("have.value","feedback");
+    cy.get("#open-text-area").should('be.visible').type( "muito obrigado pelo curso" , {delay: 0});
+    cy.get('button[type="submit"]').click();
+    cy.get('.success > strong').should("be.visible");
+  });
+  
   /*it.("CT009 enviando formulario com dados sensiveis", function (){
     const firstName = Cypress.env('user_name');
     cy.log (firstName)
